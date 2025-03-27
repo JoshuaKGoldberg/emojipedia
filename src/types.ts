d2cc03717dcd2845ca987ca25cd704ddcdfac11f
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-27 15:46:06 -0400

    chore: bump to create-typescript-app@2 with transitions action

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,31 @@
-export type UnicodeEmojiJson = typeof import("unicode-emoji-json");
+export interface ComponentShortcode {
+	code: string;
+	vendor: ComponentVendor;
+}
 
-export type UnicodeEmojiEntry = UnicodeEmojiJson[keyof UnicodeEmojiJson];
+export interface ComponentVendor {
+	slug: string;
+	title: string;
+}
+
+export interface EmojiComponent {
+	alsoKnownAs: string[];
+	appleName: string;
+	code: string;
+	codepointsHex: string[];
+	currentCldrName: string;
+	description: string;
+	id: string;
+	shortcodes: ComponentShortcode[];
+	slug: string;
+	title: string;
+}
+
+export interface EmojiShortcode {
+	code: string;
+	source: string;
+	vendor: EmojiVendor;
+}
 
 export interface EmojiV1TechnicalInformation {
 	alsoKnownAs: string[];
@@ -19,42 +44,6 @@ export interface EmojiV1TechnicalInformation {
 	version: EmojiVersion;
 }
 
-export interface EmojiVersion {
-	date: number;
-	name: string;
-	slug: string;
-	status: number;
-}
-
-export interface EmojiComponent {
-	alsoKnownAs: string[];
-	appleName: string;
-	code: string;
-	codepointsHex: string[];
-	currentCldrName: string;
-	description: string;
-	id: string;
-	shortcodes: ComponentShortcode[];
-	slug: string;
-	title: string;
-}
-
-export interface ComponentShortcode {
-	code: string;
-	vendor: ComponentVendor;
-}
-
-export interface ComponentVendor {
-	slug: string;
-	title: string;
-}
-
-export interface EmojiShortcode {
-	code: string;
-	source: string;
-	vendor: EmojiVendor;
-}
-
 export interface EmojiVendor {
 	slug: string;
 	title: string;
@@ -62,8 +51,19 @@ export interface EmojiVendor {
 
 export interface EmojiVersion {
 	date: number;
+	name: string;
+	slug: string;
+	status: number;
+}
+
+export interface EmojiVersion {
+	date: number;
 	description: string;
 	name: string;
 	slug: string;
 	status: number;
 }
+
+export type UnicodeEmojiEntry = UnicodeEmojiJson[keyof UnicodeEmojiJson];
+
+export type UnicodeEmojiJson = typeof import("unicode-emoji-json");
